86c21f81ff748afc9bdf31f3abb263f35785224b
wm0562 <1455448378@qq.com>
2019-11-14 17:54:17 +0800

    增加浏览器退出操作

--- testbag/js.py
+++ testbag/js.py
@@ -5,19 +5,21 @@ import time, os
 driver = webdriver.Chrome()
 file_path = 'file:///' + os.path.abspath('js.html')
 driver.get(file_path)
-time.sleep(5)
+time.sleep(3)
 #######通过 JS 隐藏选中的元素##########第一种方法：
 #隐藏文字信息
-js="var q=document.getElementById('tooltip').fadeOut();"
-print js
-# driver.execute_script('document.getElementByTd("tooltip")')
 # driver.execute_script('$("#tooltip").fadeOut();')
-driver.execute_script(js)
+# # driver.execute_script('document.getElementByTd("tooltip")')
+# # driver.execute_script('$("#tooltip").fadeOut();')
+# time.sleep(5)
 
 print "隐藏文字成功"
 time.sleep(5)
 #隐藏按钮：
-button = driver.find_element_by_class_name('btn')
-driver.execute_script('$(arguments[0]).fadeOut()', button)
+bu = 'document.querySelector("body > div > div > a.btn")'
+bt = bu.fadeOut()
+# button = driver.find_element_by_class_name('btn')
+driver.execute_script(bt, bu)
+# driver.execute_script('$(arguments[0]).fadeOut()', button)
 time.sleep(5)
 driver.quit()
